1f94e1a8f0e58ab277285979e1e9fbcd72efadfd
Артемий Соболев <titeha@yandex.ru>
2022-01-09 16:21:26 +0700

    Добавлена структура обмена данными

---
+++ Library/FileServices/FSDTO.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Specialized;
+
+namespace FileServices
+{
+ public struct FSDTO
+ {
+  private BitVector32 _attributes;
+
+  public string Name { get; internal set; }
+
+  public string Parent { get; internal set; }
+
+  public ulong Size { get; internal set; }
+
+  public ulong VolumeSize { get; internal set; }
+
+  public bool IsCompressed
+  {
+   get => _attributes[0];
+   internal set => _attributes[0] = value;
+  }
+
+  public bool IsDirectory
+  {
+   get => _attributes[1];
+   internal set => _attributes[1] = value;
+  }
+
+  public bool IsHidden
+  {
+   get => _attributes[2];
+   internal set => _attributes[2] = value;
+  }
+
+  public bool IsJunction
+  {
+   get => _attributes[3];
+   internal set => _attributes[3] = value;
+  }
+
+  public bool IsSystem
+  {
+   get => _attributes[4];
+   internal set => _attributes[4] = value;
+  }
+
+  public FSDTO(string name, string parent)
+  {
+   Name = name;
+   Parent = parent;
+   Size = 0;
+   VolumeSize = 0;
+   _attributes = new BitVector32(0);
+  }
+ }
+}
--- Library/FileServices/FileService.cs
+++ Library/FileServices/FileService.cs
@@ -1,7 +1,20 @@
-﻿namespace FileServices
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace FileServices
 {
  public class FileService
  {
+  private DriveInfo _drive;
 
+  public FileService(DriveInfo drive)
+  {
+   _drive = drive;
+  }
+
+  public IEnumerable<FSDTO> ScanDrive()
+  {
+
+  }
  }
 }
